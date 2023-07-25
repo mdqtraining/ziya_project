d65b9cfcbc48238c73d922e49abade274b421161
Vijay <vijayaprasath.mdq@gmail.com>
2023-07-25 19:11:48 +0530

    Responses aligned and API Integrated

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -1,39 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Chaticon from '../chatlogo.png'
 import { UserOutlined } from '@ant-design/icons';
 import { Slider } from './Slide';
-import { Carousel } from 'antd';
+import { Carousel, Col, Row, message } from 'antd';
 
-export default function ChatMessage(props) {
+export default function ChatMessage({ data, typedText, index, isLoading, messagesType, slideMessages }) {
     const onChange = ( currentSlide ) => {
         console.log(currentSlide);
       };
+    const [currentIndex, setCurrentIndex] = useState(0);
+      
+    const SlideContent = ({ dataset }) => {
+        const slideItems = dataset.map((item) => (
+            <>
+                <h4 style={{ color: '#E6D9CD' }}>{data}</h4>
+                <div key={item.id} style={{ display: 'flex', flexDirection: 'column' }}>
+                    {Object.entries(item).map(([key, value]) => (
+                        <div key={key}>
+                            <h4 style={{ color: '#F9F9F8' }}>{key}: </h4>
+                            <h4 style={{ color: '#B1E6E4' }}>{value}</h4>
+                        </div>
+                    ))}
+                </div>
+                <h5 style={{ color: '#E6D9CD' }}>Please let me know if you would like more information about any of these positions or if you have any other questions."</h5>
+            </>
+        ));
+
+        return (
+            <Carousel afterChange={onChange}>
+                {slideItems}
+            </Carousel>
+        );
+    };
+
+
+    const MainContent = ({ dataset }) => {
+        return (
+            <div>
+                <h3 className="contentStyle">{dataset}</h3>
+            </div>
+        );
+    };
+
     return (
-        <div className='parrent-msg'>
-            {
-                props.user ? (
-                    <div className='message-right'>
-                        <span className='message-text'>{props.message}</span>
-                        <UserOutlined className='message-icon' />
+        <div className="parrent-msg">
+            <Row>
+                <Col span={24} style={{ textAlign: 'right' }}>
+                    <div className="message-right">
+                        <span className="message-text">{typedText[index]?.text}</span>
+                        <UserOutlined className="message-icon" />
                     </div>
-                ) :
-                    <div className='message-left'>
-                        <img src={Chaticon} className='message-icon' width={50} />
-                        {/* <span className='message-text msg-txt-left'>{props.message}</span> */ }
-                        <div>
-                        <Carousel afterChange={onChange}>
 
-                            <div>
-                                <h3 className=" contentStyle">{props.message}</h3>
-                            </div>
-                            <div>
-                                <h3 className=" contentStyle">{props.message}</h3>
-                            </div>
-                        </Carousel>
-                       </div>
+                </Col>
+            </Row>
+            <Row>
+                <Col span={24}>
+                    <div className="message-left">
+                        <img src={Chaticon} className="message-icon" width={50} />
+                        <div className="fullcarousel">
+                            {messagesType === 'slide' && slideMessages[currentIndex] ? (
+                                <SlideContent dataset={slideMessages[currentIndex]} />
+                            ) : (
+                                <MainContent dataset={data} />
+                            )}
+                        </div>
                     </div>
-            }
+                </Col>
+            </Row>
         </div>
-    )
-
+    );
 }
+
+
--- src/components/Chatbot.js
+++ src/components/Chatbot.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import ChatMessage from './ChatMessage'
 import { Input } from 'antd';
-// import { Slider } from './Slide';
+import Modal from './Modal'
 
 import { Col, Row } from 'antd';
 import { SendOutlined } from '@ant-design/icons'
@@ -15,10 +15,15 @@ export default function Chatbot() {
     const [status, setStatus] = useState(false);
     const [color, setColor] = useState(false);
     const [messages, setMessage] = useState([]);
-
-
+    const [slideMessages, setSildeMessage] = useState([]);
+    const [messagesType, setMessageType] = useState('');
     const [text, setText] = useState('');
+    const [typedTexts , setTypedTexts] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+   
     const onSend = () => {
+        setTypedTexts((prevTypedTexts) => [...prevTypedTexts,  {text}]);
         const sendData = new FormData();
         sendData.append('Body', JSON.stringify(text))
                 axios({
@@ -32,44 +37,72 @@ export default function Chatbot() {
                             setOpen(true);
                             setStatus(false);
                             setColor(false);
-                            console.log(res.data.response);
+                            setIsLoading(false);
                         } else {
                             setMessage(res.data.message);
                             setOpen(true);
                             setStatus(true);
                             setColor(true);
-                            console.log(res.data.response);
-                            setMessage([...messages, res.data])
+                            if(res.data.type === 'slide'){
+                            setMessageType('slide');
+                                setSildeMessage((prevSlideMessages) => {
+                                    return { ...prevSlideMessages, [messages.length]: JSON.parse(res.data.data) };
+                                });
+                                setMessage([...messages, res.data.response]); 
+                            }
+                            else{
+                            setMessage([...messages, res.data.response]); 
+                            setMessageType('text');
+                            }
+                            setIsLoading(false);
                             setText("")
                             setTimeout(() => {
-                                document.querySelector('#copyright').scrollIntoView();
+                                document.querySelector('#resBody').scrollIntoView();
                             }, 1);
-                        }
-                    })
-                    .catch((err) => {
-                        alert("Oops something went wrong " + err);
-                    });
+                          }
+                         })
+                        .catch((err) => {
+                         alert("Oops something went wrong " + err);
+                        });
                 };
 
 
     return (
-        <div>
+        <div className='connect'>
+            <Row>
+            <Col span={6} style={{ display:'flex', alignItems:'center', justifyContent:'center'}}>
             <div className='logo-head'>
                 <img src={clogo} alt='...' width="60" />
                 <h4>Ziya</h4>
             </div>
-            <div className='chat-message'>
-                {
-                    messages.length > 0 && messages.map((data) => <ChatMessage {...data} />)
-                  
-                } 
+            </Col>
+            <Col span={18} style={{ display:'flex', alignItems:'center'}}>
+                <div>
+                <h3 className='con-text'>Connecting Talents with Opportunities</h3>
+                </div>
+            </Col>
+            </Row>
+            <div className='chat-message' id="resBody">
+                {messages.length > 0 &&
+                    messages.map((data, index) => (
+                        <ChatMessage
+                            key={index}
+                            data={data}
+                            typedText={typedTexts}
+                            index={index}
+                            isLoading={isLoading}
+                            messagesType={messagesType}
+                            slideMessages={slideMessages}
+                        />
+                    ))}
                  {/* <Slider /> */}
             </div>
 
             <Row className='tex-bar'>
-                <Col span={12} offset={6} className='text-bar' id="copyright">
+                <Col span={12} offset={6} className='text-bar'>
+                    <Modal />
                     {/* <div className='text-bar' id='copyright'> */}
-                    <Input className='input-bar' placeholder="Hi, How can i assist you?(Write here)" value={text} onChange={(e) => setText(e.target.value)} />
+                    <Input className='input-bar' placeholder="Send a Message" value={text} onChange={(e) => setText(e.target.value)} />
                     <button className='send-btn' onClick={onSend}><SendOutlined style={{ color: 'black' }} /></button>
                     {/* </div> */}
                 </Col>
